1a76a47505495ff3a6f2d351726497690c53c163
Vijay Lakshmanan <vijay.ellu@gmail.com>
2020-05-30 16:31:22 +0800

    Added all the code to get a weather for a location
    Currently it just displays the json returned from
    openweathermap

--- app/src/main/java/com/example/whatstheweather/MainActivity.java
+++ app/src/main/java/com/example/whatstheweather/MainActivity.java
@@ -2,13 +2,96 @@ package com.example.whatstheweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView result;
+    EditText cityName;
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+                InputStream stream = urlConnection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                    Log.i("Response", ">" + line);
+                }
+                return buffer.toString();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.i("Result post execute", s);
+            result.setText(s);
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getWeather(View view) {
+        cityName = (EditText) findViewById(R.id.weatherCity);
+        result = (TextView) findViewById(R.id.weatherResult);
+
+        Log.i("City Name", cityName.getText().toString());
+        DownloadTask weather = new DownloadTask();
+        String encodedCityName = null;
+        try {
+            encodedCityName = URLEncoder.encode(cityName.getText().toString(), "UTF-8");
+            weather.execute("http://api.openweathermap.org/data/2.5/weather?q=" + encodedCityName +
+                    "&appid=6eef11aa2c76d993bd46f0cfd20a6393");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
